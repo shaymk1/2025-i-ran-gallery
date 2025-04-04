665ae9d87b2884e9363448cd5db7e98ebfceca4c
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-04-04 22:44:11 +0200

    created models, registered them and added data in the admin panel using models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    month = models.CharField(max_length=100)
+    venue = models.CharField(max_length=100)
+    race = models.IntegerField()
+    slug = models.SlugField(unique=True)
+
+    # ensures that the slug is autaomatically generated from the month and venue fields
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.month + "-" + self.venue)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.month} - {self.venue}"
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name="photos"
+    )
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="photos/")
+
+    def __str__(self):
+        return self.title
