1ecd9f88cce1990177c55a5079f545161ff0c833
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-04-04 22:44:11 +0200

    created models, registered them and added data in the admin panel using models

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core.settings.base import MEDIA_ROOT
 
 urlpatterns = [
     path("admin/", admin.site.urls),
