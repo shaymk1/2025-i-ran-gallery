482b7af23bfb8fc51451421a64761d26ed8b119f
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-04-04 19:03:30 +0200

    moved static folder into the app, configured images and links with jinja templates

--- app/views.py
+++ app/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def blog(request):
+    return render(request, "blog.html")
--- core/settings/base.py
+++ core/settings/base.py
@@ -105,11 +105,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "static/"
+MEDIA_URL = "media/"
+
+# WhiteNoise settings
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
