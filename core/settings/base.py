8fb2f442bed233f79aa0f20ae8c9e65b046aea80
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-04-15 14:43:50 +0200

    done with the render.yaml file, modified .env and base settings to get them ready for render

--- core/settings/base.py
+++ core/settings/base.py
@@ -18,10 +18,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
-ALLOWED_HOSTS = [
-    "*",
-]
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
