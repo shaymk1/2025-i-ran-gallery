28e007d8b7ae88a20bbecd35f022e704bc41b101
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-04-17 14:12:03 +0200

    the cloudinary in settings was giving me errors on render, so i delayed the start of cloudinary config until the app is ready by moving the cloudinary-config code and imports from base settings to app.py

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+import cloudinary
+from decouple import config
 
 
 class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app"
+
+    # to delay cloudinary config until the app is ready
+    def ready(self):
+        cloudinary.config(secure=True, cloudinary_url=config("CLOUDINARY_URL"))
