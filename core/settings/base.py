b6803d1cb1d7c0d5dedba67d27d3ad5d721be887
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-04-23 11:58:33 +0200

    updated base settings to comment out staticfiles_storage

--- core/settings/base.py
+++ core/settings/base.py
@@ -179,7 +179,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # aws s3 settings
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
