30c4d496bb1bde9429caee0052b134e2120e436c
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-05-07 12:56:25 +0200

    created a backup db , and loaded postgresql credentials in settings.py and in the .env, deleted some old db backups and edited one db backup_2025-04-29

--- core/settings.py
+++ core/settings.py
@@ -118,12 +118,21 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
+# DATABASES = {
+#      'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+#      }
 # Email Configuration (Gmail example)smtp
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
