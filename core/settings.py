822fe4b32ec1831ec369c12b6f31d262c63c4cd4
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-05-10 14:11:22 +0200

    used RDS for database and then configured it in the settings

--- core/settings.py
+++ core/settings.py
@@ -129,14 +129,25 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST", "localhost"),
+#         "PORT": os.getenv("DB_PORT", "5432"),
+#     }
+}
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("RDS_DB_NAME"),
+        "USER": os.getenv("RDS_USER"),
+        "PASSWORD": os.getenv("RDS_PASSWORD"),
+        "HOST": os.getenv("RDS_DB_HOST"),
+        "PORT": os.getenv("RDS_PORT", "5432"),
     }
 }
 # DATABASES = {
