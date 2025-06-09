300fffb310c7c2f6c7cecdcde309b8f449398b33
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-06-09 21:32:34 +0200

    changed the images urls in index, blog, about to come from image.url instead of optimized_image.url, remove the cache configuration in the core url,added a media context processor in the settings,.py

--- core/settings.py
+++ core/settings.py
@@ -94,6 +94,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                'django.template.context_processors.media',  
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
--- core/urls.py
+++ core/urls.py
@@ -2,16 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import os
+# import os
 
 # from core.settings.base import MEDIA_ROOT
 
 urlpatterns = [
-    #path("admin/", include("admin_honeypot.urls", namespace="admin_honeypot")),
+    # path("admin/", include("admin_honeypot.urls", namespace="admin_honeypot")),
     path(
         "secure-dashboard-2025/", admin.site.urls
     ),  # admin site url changed to "secure-dashboard-2025"
-    
     path("", include("app.urls")),
 ]
 
@@ -20,4 +19,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static('/media/CACHE/', document_root=os.path.join(settings.MEDIA_ROOT, 'CACHE'))
+    # urlpatterns += static(
+    #     "/media/CACHE/", document_root=os.path.join(settings.MEDIA_ROOT, "CACHE")
+    # )
